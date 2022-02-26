c3e57443c433563b096f66199af7532678cda0e6
lamnelson <87283839+lamnelson@users.noreply.github.com>
2022-02-26 04:10:37 -0600

    Website v1.0

---
+++ script/app.js
@@ -0,0 +1,42 @@
+// Typewriter created by Tameem Safi
+// source: https://github.com/tameemsafi/typewriterjs
+var app = document.getElementById('typewriter');
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 40,
+    deleteSpeed: 4
+});
+
+typewriter.typeString('Hello!')
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString('Welcome to my Web Portfolio.')
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("I am a student at UT Arlington.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("I am majoring in Software Engineering")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("I am interested in front-end developemnt & UI/UX.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("I am based in Dallas, TX.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("Contact me for further inquiries!")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("Thank you for visiting!")
+    .pauseFor(1000)
+    .deleteAll()
+    .start();
